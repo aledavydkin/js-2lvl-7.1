4a8168186ac160201445b27486a9cabad2b0d046
Alex <alexandr.davyidkin@eldorado.ru>
2020-05-22 10:13:01 +0500

    1.2 Задание

--- src/js/characters/character.js
+++ src/js/characters/character.js
@@ -10,7 +10,7 @@ class Character {
       throw new Error('нельзя повысить левел умершего');
     }
     this.lvl += 1;
-    this.health *= 100;
+    this.health = 100;
     this.attack *= 1.2;
     this.defense *= 1.2;
   }
